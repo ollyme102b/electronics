acf450e9744f0f7318c256cb2b33ca76c193b782
Ryan Cosner <rkcosner@gmail.com>
2019-04-16 16:52:25 -0700

    Added new code which should, in theory, make it so that the array inputted is in meters/second

--- DriveTrain/OmniWheels/3MotorswUnits/3MotorswUnits.ino
+++ DriveTrain/OmniWheels/3MotorswUnits/3MotorswUnits.ino
@@ -6,6 +6,9 @@ double wheelR = 0.051; // 5.1 cm CHANGED RADIUS VALUE
 double wheelF [3]; // Frequency
 double wheelT [3]; // Period
 double wheelDir [3]; // direction 
+double time1; // to control speed
+double time2; // to control speed
+double stillWheel [3] = {0,0,0};
 
 
 
@@ -31,9 +34,11 @@ void setup() {
     // So input here is in
     wheelF[i] = speeds[i]/wheelR; //radians per second
     if(abs(wheelF[i])<0.05){
-      wheelF[i] = 0.05;           //This line replaces any zero values with 0.05, thus stopping it from having any divide by zero errors
+      stillWheel[i] = 1;          //Indicator showing that the ith wheel isn't moivng while in this configuration 
+      wheelT[i] = 1;              //So that when finding the overall period for the system, this still wheel doesn't have an effect
+    }else{
+      wheelT[i] = round(1000*abs(1/wheelF[i]))*2*pi/200; // Now in rounded milliseconds per step
     }
-    wheelT[i] = round(100*abs(1/wheelF[i]))/100;
     wheelDir[i] = wheelF[i]>0;
   }
   Serial.print(wheelT[0]);
@@ -47,24 +52,38 @@ void loop() {
   digitalWrite(2, boolean(wheelDir[2]));
   
   for (int i = 0; i < wheelT[0]*wheelT[1]*wheelT[2]; i++) {
-      if (i%(int(wheelT[0])*2) < wheelT[0]){
+    time1 = micros(); // Get actuation start time
+    
+    if (wheelStill[1] == 0){
+      if (i%(int(wheelT[0]*2)) < wheelT[0]){  //multiplied by two because the loop iterates at 0.5 milliseconds
         digitalWrite(7, HIGH);
       }else{
         digitalWrite(7, LOW);
       }
-      
-      if (i%(int(wheelT[1])*2) < wheelT[1]){
+    }
+
+    if (wheelStill[2] == 0){
+      if (i%(int(wheelT[1]*2)) < wheelT[1]){
         digitalWrite(5, HIGH);
       }else{
         digitalWrite(5, LOW);
       }
-      
-      if (i%(int(wheelT[2])*2) < wheelT[2]){
+    }
+
+    if (wheelStill[3] == 0){
+      if (i%(int(wheelT[2]*2)) < wheelT[2]){
         digitalWrite(3, HIGH);
       }else{
         digitalWrite(3, LOW);
       }
-      delayMicroseconds(500);
+    }
+
+    time2 = micro(); // Get actuation end time
+    if (time1 > time2 ){
+      time2 = time1; // Correct for modulus measurement error
+    }
+    
+    delayMicroseconds(500 - (time2 - time1)); // 500 microseconds and the smallest resolution is 1 millisecond step. Also, adjust for the time lost in actuation
 
   }
   
