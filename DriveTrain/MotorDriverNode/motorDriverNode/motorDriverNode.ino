0fc3fcb36fccbe787c677d0c26abd57f5e1339d8
Rohan Sinha <rohan.sinha@berkeley.edu>
2019-04-19 18:04:05 -0700

    modified node

--- DriveTrain/MotorDriverNode/motorDriverNode/motorDriverNode.ino
+++ DriveTrain/MotorDriverNode/motorDriverNode/motorDriverNode.ino
@@ -1,6 +1,7 @@
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Vector3.h>
+#include <std_msgs/Int32.h>
 double vector [3] = {0.0, 0.0, 0.0}; // Reference Vector: [Vx, Vy, Omega] 
 double dynamics [3][3] = {{0.866, 0.5, -0.5},{0.0, -1.0, -0.5},{-0.866, 0.5, -0.5}}; // Linear Transform [w1,w2,w3] = Av
 double speeds [3]={0,0,0};
@@ -10,6 +11,13 @@ double wheelT [3]= {0.0, 0.0, 0.0}; // Period
 double wheelDir [3] = {0.0, 0.0, 0.0}; // direction 
 
 void messageCb(const geometry_msgs::Twist& toggle_msg) {
+  vector[0] = toggle_msg.linear.x;
+  vector[1] = toggle_msg.linear.y;
+  vector[2] = toggle_msg.angular.z;
+  for (int k = 0; k<3; k++) {
+    speeds[k] = 0;
+  } 
+  
   for(int r = 0; r<3; r++){
     for(int c = 0; c<3; c++){
       speeds[r] = speeds[r] + dynamics[r][c]*vector[c];
@@ -29,8 +37,10 @@ void messageCb(const geometry_msgs::Twist& toggle_msg) {
 /* 
  * setup publisher and subscriber
  */
-ros::NodeHandle nh;
+ros::NodeHandle_<ArduinoHardware,1,2,200,150> nh;
+std_msgs::Int32 debug_msg;
 ros::Subscriber<geometry_msgs::Twist> sub("velocity_cmd", &messageCb);
+ros::Publisher chatter("debug", &debug_msg);
 
 /* 
  * setup the node 
@@ -46,13 +56,16 @@ void setup()
   
   nh.initNode();
   nh.subscribe(sub);
+  nh.advertise(chatter);
 }
 
 /* 
  * run update code
  */
 void loop()
-{
+{  
+  debug_msg.data = wheelT[0]*wheelT[1]*wheelT[2];
+  chatter.publish(&debug_msg);
   digitalWrite(6, boolean(wheelDir[0])); 
   digitalWrite(4, !boolean(wheelDir[1]));
   digitalWrite(2, boolean(wheelDir[2]));
@@ -75,8 +88,11 @@ void loop()
         digitalWrite(3, LOW);
       }
       delayMicroseconds(500);
-
+      
   }
+  debug_msg.data=1;
+  chatter.publish(&debug_msg);
   nh.spinOnce();
+  
 }
 
